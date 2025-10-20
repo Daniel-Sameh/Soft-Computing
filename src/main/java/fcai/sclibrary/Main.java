b8a5e3e6f0f2f5bf29591e9950f312fa4ca6e0ec
Marcelino-10 <marcelinoebied@gmail.com>
2025-10-21 00:21:52 +0300

    updating the logic of mutation methods

--- src/main/java/fcai/sclibrary/Main.java
+++ src/main/java/fcai/sclibrary/Main.java
@@ -1,7 +1,18 @@
 package fcai.sclibrary;
 
+import fcai.sclibrary.ga.chromosome.Chromosome;
+import fcai.sclibrary.ga.chromosome.factory.BinaryChromosomeFactory;
+import fcai.sclibrary.ga.chromosome.factory.Range;
+import fcai.sclibrary.ga.operators.mutation.FlipMutation;
+import fcai.sclibrary.ga.operators.mutation.Mutation;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BinaryChromosomeFactory factory = new BinaryChromosomeFactory();
+
+        Chromosome<Integer> chromosome = factory.createRandomChromosome(10, new Range<Integer>(0, 1), null);
+        Mutation<Integer> m = new FlipMutation();
+        m.mutate(chromosome, 0.5, new Range<Integer>(0, 1));
+
     }
 }
--- src/main/java/fcai/sclibrary/ga/operators/mutation/NonUniformMutation.java
+++ src/main/java/fcai/sclibrary/ga/operators/mutation/NonUniformMutation.java
@@ -6,35 +6,45 @@ import fcai.sclibrary.ga.chromosome.factory.Range;
 import java.util.List;
 import java.util.Random;
 
-public class NonUniformMutation implements Mutation {
+public class NonUniformMutation implements Mutation<Double> {
     @Override
-    public void mutate(Chromosome chromosome, int pm, Range range){
+    public Chromosome<Double> mutate(Chromosome<Double> chromosome, double pm, Range<Double> range){
         List<Double> genes = chromosome.getGenes();
-        double lb = range.getLower().doubleValue(), ub = range.getUpper().doubleValue();
+        double lb = range.getLower(), ub = range.getUpper();
+        double min = 0.0;
+        double max = 1.0;
+        Random rand = new Random();
+        boolean changed = false;
         for(int i = 0; i < genes.size(); i++){
-            double lxi = genes.get(i) - lb;
-            double uxi = ub - genes.get(i);
-            double min = 0.0;
-            double max = 1.0;
-            Random rand = new Random();
-            double r1 = min + (rand.nextDouble() * (max - min));
-            double y;
-            if(r1 <= 0.5){
-                y = lxi;
-            }
-            else {
-                y = uxi;
-            }
-            double r = min + (rand.nextDouble() * (max - min));
-            double b = 0.5;
-            double t = 1, T = 1;
-            double delta =y * (1 - Math.pow(r, Math.pow(((1 - t) / T), b)));
-            if(y == lxi){
-                genes.set(i, genes.get(i) - delta);
-            }
-            else if(y == uxi){
-                genes.set(i, genes.get(i) + delta);
+            double rm = min + (rand.nextDouble() * (max - min));
+            if(rm <= pm){
+                changed = true;
+                double lxi = genes.get(i) - lb;
+                double uxi = ub - genes.get(i);
+                double r1 = min + (rand.nextDouble() * (max - min));
+                double y;
+                if(r1 <= 0.5){
+                    y = lxi;
+                }
+                else {
+                    y = uxi;
+                }
+                double r = min + (rand.nextDouble() * (max - min));
+                double b = 0.5;
+                double t = 1, T = 1;
+                double delta =y * (1 - Math.pow(r, Math.pow(((1 - t) / T), b)));
+                if(y == lxi){
+                    genes.set(i, genes.get(i) - delta);
+                }
+                else if(y == uxi){
+                    genes.set(i, genes.get(i) + delta);
+                }
             }
         }
+        chromosome.setGenes(genes);
+        if(!changed)
+            return null;
+        else
+            return chromosome;
     }
 }
