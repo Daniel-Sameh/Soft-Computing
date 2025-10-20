2c82bfaf1ccd9285e345f313a7af6e3cd07614e8
YoussefEhab2 <youka961@gmail.com>
2025-10-21 00:22:36 +0300

    feat:added three crossover methods: single point,N point,Order1

--- src/main/java/fcai/sclibrary/Main.java
+++ src/main/java/fcai/sclibrary/Main.java
@@ -1,5 +1,18 @@
 package fcai.sclibrary;
 
+import fcai.sclibrary.ga.chromosome.BinaryChromosome;
+import fcai.sclibrary.ga.chromosome.Chromosome;
+import fcai.sclibrary.ga.chromosome.IntegerChromosome;
+import fcai.sclibrary.ga.chromosome.factory.BinaryChromosomeFactory;
+import fcai.sclibrary.ga.chromosome.factory.ChromosomeFactory;
+import fcai.sclibrary.ga.chromosome.factory.IntegerChromosomeFactory;
+import fcai.sclibrary.ga.chromosome.factory.Range;
+import fcai.sclibrary.ga.operators.crossover.*;
+import java.util.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- src/main/java/fcai/sclibrary/ga/operators/crossover/Crossover.java
+++ src/main/java/fcai/sclibrary/ga/operators/crossover/Crossover.java
@@ -1,4 +1,11 @@
 package fcai.sclibrary.ga.operators.crossover;
 
-public interface Crossover {
+import fcai.sclibrary.ga.chromosome.Chromosome;
+import fcai.sclibrary.ga.chromosome.factory.Range;
+
+import java.util.List;
+
+public interface Crossover <T extends Number>{
+
+    public List<Chromosome<T>> crossover(Chromosome<T> chromosome1,Chromosome<T> chromosome2,double pc);
 }
