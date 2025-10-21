b03aa48a585cc5079fe16bcd3ddffecb8869b7aa
Abanob Essam <abanobessam19@gmail.com>
2025-10-21 19:30:48 +0300

    Feat: Adding the replacement logic

---
+++ src/main/java/fcai/sclibrary/ga/operators/replacement/functions/CutoffFunction.java
@@ -0,0 +1,14 @@
+package fcai.sclibrary.ga.operators.replacement.functions;
+
+import fcai.sclibrary.ga.chromosome.Chromosome;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class CutoffFunction<T extends Number> implements ReplacementFunction<T> {
+    @Override
+    public List<Chromosome<T>> select(List<Chromosome<T>> ls, int size) {
+        ls.sort(Comparator.comparingDouble((Chromosome<T> c) -> c.getFitness()).reversed());
+        return ls.subList(0, size);
+    }
+}
