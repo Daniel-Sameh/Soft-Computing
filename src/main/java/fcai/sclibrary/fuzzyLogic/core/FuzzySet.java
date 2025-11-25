dabd3428ede37cef8fae889033c3303d3c461457
Abanob Essam <abanobessam19@gmail.com>
2025-11-25 18:27:36 +0200

    Feat: adding the fuzzifier logic.

--- src/main/java/fcai/sclibrary/fuzzyLogic/core/FuzzySet.java
+++ src/main/java/fcai/sclibrary/fuzzyLogic/core/FuzzySet.java
@@ -16,14 +16,7 @@ import java.util.Map;
 public class FuzzySet {
     private String type;
     private String name;
-    private class Range{
-        private Integer start, end;
-    }
-    private class Line{
-        private Double slope, intercept;
-    }
-    private Map<Range, Line> linesEquations;
-    private List<Integer> indices;
+    private List<Double> indices;
 
     private Double value;
 }
