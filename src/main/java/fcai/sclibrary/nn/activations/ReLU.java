4febfa6effd92ce03be768451cec821d54113e2a
Abanob Essam <abanobessam19@gmail.com>
2025-12-16 13:45:37 +0200

    Feat: Added the Activation functions

--- src/main/java/fcai/sclibrary/nn/activations/ReLU.java
+++ src/main/java/fcai/sclibrary/nn/activations/ReLU.java
@@ -1,4 +1,15 @@
 package fcai.sclibrary.nn.activations;
 
-public class ReLU {
+public class ReLU implements ActivationFunction {
+    public ReLU() {}
+
+    @Override
+    public double forward(double x) {
+        return x < 0 ? 0.0 : x;
+    }
+
+    @Override
+    public double derivative(double x) {
+        return x < 0 ? 0.0 : 1.0;
+    }
 }
--- src/main/java/fcai/sclibrary/nn/activations/Tanh.java
+++ src/main/java/fcai/sclibrary/nn/activations/Tanh.java
@@ -1,4 +1,15 @@
 package fcai.sclibrary.nn.activations;
 
-public class Tanh {
+public class Tanh implements ActivationFunction {
+    public Tanh() {}
+
+    @Override
+    public double forward(double x) {
+        return (2 / (1 + Math.exp(-2 * x))) - 1;
+    }
+
+    @Override
+    public double derivative(double x) {
+        return 1 - Math.pow(forward(x), 2);
+    }
 }
